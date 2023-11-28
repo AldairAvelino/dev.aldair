5c890b124df3297572871f5cdbe0963d46320eca
Aldair Avelino <aldair03avelino@gmail.com>
2023-11-28 23:22:26 +0100

    Implemented Contact Section - AA

---
+++ src/components/sections/contact.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import { srConfig, email } from '@config';
+import sr from '@utils/sr';
+import { usePrefersReducedMotion } from '@hooks';
+
+const StyledContactSection = styled.section`
+  max-width: 600px;
+  margin: 0 auto 100px;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    margin: 0 auto 50px;
+  }
+
+  .overline {
+    display: block;
+    margin-bottom: 20px;
+    color: var(--purple);
+    font-family: var(--font-mono);
+    font-size: var(--fz-md);
+    font-weight: 400;
+
+    &:before {
+      bottom: 0;
+      font-size: var(--fz-sm);
+    }
+
+    &:after {
+      display: none;
+    }
+  }
+
+  .title {
+    font-size: clamp(40px, 5vw, 60px);
+  }
+
+  .email-link {
+    ${({ theme }) => theme.mixins.bigButton};
+    margin-top: 50px;
+  }
+`;
+
+const Contact = () => {
+  const revealContainer = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
+  return (
+    <StyledContactSection id="contact" ref={revealContainer}>
+      <h2 className="numbered-heading overline">Next Adventure Awaits!</h2>
+
+      <h2 className="title">Let's Connect!</h2>
+
+      <p>
+        Currently, I'm available for new opportunities and would be delighted to receive your
+        message. Feel free to get in touch if you need additional information or just for a casual
+        conversation. Can't wait to hear from you!
+      </p>
+
+      <a className="email-link" href={`mailto:${email}`}>
+        Send Me An E-mail
+      </a>
+    </StyledContactSection>
+  );
+};
+
+export default Contact;
