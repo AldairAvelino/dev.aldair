ada2c55d0c7f153a6f37664a4ad6935273e03678
Aldair Avelino <aldair03avelino@gmail.com>
2023-11-28 13:32:14 +0100

    Implemented Hero Section with Transition Effects - AA

---
+++ src/components/sections/hero.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import styled from 'styled-components';
+import { navDelay, loaderDelay } from '@utils';
+import { usePrefersReducedMotion } from '@hooks';
+
+const StyledHeroSection = styled.section`
+  ${({ theme }) => theme.mixins.flexCenter};
+  flex-direction: column;
+  align-items: flex-start;
+  min-height: 100vh;
+  height: 100vh;
+  padding: 0;
+
+  @media (max-height: 700px) and (min-width: 700px), (max-width: 360px) {
+    height: auto;
+    padding-top: var(--nav-height);
+  }
+
+  h1 {
+    margin: 0 0 30px 4px;
+    color: var(--purple);
+    font-family: var(--font-mono);
+    font-size: clamp(var(--fz-sm), 5vw, var(--fz-md));
+    font-weight: 400;
+
+    @media (max-width: 480px) {
+      margin: 0 0 20px 2px;
+    }
+  }
+
+  h3 {
+    margin-top: 5px;
+    color: var(--slate);
+    line-height: 0.9;
+  }
+
+  p {
+    margin: 20px 0 0;
+    max-width: 540px;
+  }
+
+  .email-link {
+    ${({ theme }) => theme.mixins.bigButton};
+    margin-top: 50px;
+  }
+`;
+
+const Hero = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    const timeout = setTimeout(() => setIsMounted(true), navDelay);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const one = <h1>Heey Folks! My name is</h1>;
+  const two = <h2 className="big-heading">Aldair Avelino</h2>;
+  const three = <h3 className="big-heading">I build web applications.</h3>;
+  const four = (
+    <>
+      <p>
+        I am a junior software developer that can design and build stunning applications, featuring
+        aesthetically pleasing designs and providing an exceptional user experience. digital
+        experiences. I also write short concise technology articles to share my knowledge. Feel free
+        to read them and share your perspective
+        {/* {' '} 
+        <a href="https://medium.com/@aldair03avelino" target="_blank" rel="noreferrer">
+          Medium
+        </a> {' '}
+        or at{' '}
+        <a href="https://www.tabnews.com.br/AldairAvelino" target="_blank" rel="noreferrer">
+          TabNews
+        </a> */}
+        .
+      </p>
+    </>
+  );
+  const five = (
+    <a
+      className="email-link"
+      href="https://www.tabnews.com.br/AldairAvelino"
+      target="_blank"
+      rel="noreferrer">
+      Read My Articles!
+    </a>
+  );
+
+  const items = [one, two, three, four, five];
+
+  return (
+    <StyledHeroSection>
+      {prefersReducedMotion ? (
+        <>
+          {items.map((item, i) => (
+            <div key={i}>{item}</div>
+          ))}
+        </>
+      ) : (
+        <TransitionGroup component={null}>
+          {isMounted &&
+            items.map((item, i) => (
+              <CSSTransition key={i} classNames="fadeup" timeout={loaderDelay}>
+                <div style={{ transitionDelay: `${i + 1}00ms` }}>{item}</div>
+              </CSSTransition>
+            ))}
+        </TransitionGroup>
+      )}
+    </StyledHeroSection>
+  );
+};
+
+export default Hero;
